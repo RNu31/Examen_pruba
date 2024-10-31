b81b145b0912197a1ef98b2dd281961de9833de7
RNu31 <143842642+RNu31@users.noreply.github.com>
2024-10-30 23:56:48 -0600

    merge::Clubes a Miembros

--- main.cc
+++ main.cc
@@ -17,6 +17,18 @@ struct Club
   std::queue<Miembro> Miembros;
 };
 
+bool idExistente(const std::vector<Club>& clubes, const std::string& id){
+    for (const Club& club : clubes) {
+        for (const Miembro& miembro : club.Miembros) {
+            if (miembro.id == id) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+void RegistrarMiembro(std::queue<Club>& clubes);
+void ListarMiembros(const std::queue<Club>& clubes);
 void CrearClubes(std::queue<Club>& Clubes);
 void MostrarClubes(std::queue<Club>& Clubes);
 void EliminarClubes(std::queue<Club>& Clubes);
@@ -103,4 +115,60 @@ void Menu(){
   std::cout << "3. Eliminar Clubes\n";
   std::cout << "4. Salir\n";
   std::cout << "Seleccione una opcion: ";
+}
+void RegistrarMiembro(std::vector<Club>& clubes, int n){
+    if (clubes.empty()){
+        std::cout << "No hay clubes disponibles para registar nuevos miembros.\n";
+        return;
+    }
+    MostrarClubes(clubes);
+    std::cout << "\nSeleccione el numero del club";
+    std::cin >> n;
+    if (n < 1 || n > clubes.size()){
+        std::cout << "Numero de club invalido.\n";
+        return;
+    }
+    Miembro nuevoMiembro;
+    std::cout << "Ingrese el nombre del nuevo miembro: ";
+    std::getline(std::cin, nuevoMiembro.nombre);
+    std::cout << "Ingrese el apellido del nuevo miembro: ";
+    std::getline(std::cin, nuevoMiembro.apellido);
+    std::cout << "Ingrese la edad del nuevo miembro: ";
+    std::cin >> nuevoMiembro.edad;
+    std::cin.ignore();
+
+    do{
+        std::cout  << "Ingrese el id del nuevo miembro: ";
+        std::getline(std::cin, nuevoMiembro.id);
+        if (idExistente(clubes, nuevoMiembro.id)){
+            std::cout << "El id ya existe, por favor ingrese otro\n";
+        } 
+    }while (idExistente(clubes, nuevoMiembro.id));
+    clubes[n - 1].Miembros.push_back(nuevoMiembro);
+    std::cout << "Miembro registrado exitosamente!\n";
+}
+
+void ListarMiembros(const std::vector<Club>& clubes, int n){
+    if (clubes.empty())
+    {
+        std::cout  << "No hay ningun club registrado.\n";
+        return;
+    }
+    MostrarClubes(clubes);
+    std::cout << "\nSeleccion el numero del club: ";
+    std::cin >> n;
+    if (n < 1 || n > clubes.size()){
+        std::cout << "Numero de club invalido.\n";
+        return;
+    }
+    const Club& club = clubes[n - 1];
+    if (club.Miembros.empty()) {
+        std::cout << "No hay miembros registrados en este club.\n";
+        return;
+    }
+    std::cout << "\nMiembros del club " << club.nombre << ":\n";
+    for (const auto& miembro : club.Miembros) {
+        std::cout << miembro.nombre << " " << miembro.apellido << " " <<
+        miembro.edad << " " << miembro.id << "\n";
+    }
 }
